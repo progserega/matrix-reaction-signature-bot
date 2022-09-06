11fe0385300e473c43916fde60b73000ed47ec44
progserega <progserega@gmail.com>
2022-09-06 17:34:00 +1000

    отображение устаревших замечаний

--- commands.py
+++ commands.py
@@ -181,9 +181,10 @@ async def process_command(room,event,commandline):
   4. show_signature - show signature for user
   5. add_rule_interruption - increment rule interruption
   6. show_rule_interruption_stat - show active/all rule interruption count for user
-  6. show_active_rule_interruption - show active rule interruption description (for all interruption) - be cafule with many active interruption for user - bot will spam messages
-  7. clear_active_rule_interruption - clear active rule interruption count (when unban user)
-  8. set_locale - change language of bot for this room
+  6. show_active_rule_interruption_descr - show active rule interruption description (for all interruption) - be cafule with many active interruption for user - bot will spam messages
+  7. show_old_rule_interruption_descr - show old (not active) rule interruption description (for all old interruption) - be cafule with many old not active interruption for user - bot will spam messages
+  8. clear_active_rule_interruption - clear active rule interruption count (when unban user)
+  9. set_locale - change language of bot for this room
       """)
       if await matrix_api.send_text(room,help_text) == False:
         log.error("matrix_api.send_text()")
@@ -650,18 +651,18 @@ async def process_command(room,event,commandline):
           return False
         return True
 
-    elif command == "show_active_rule_interruption" or command == _("show_active_rule_interruption"):
+    elif command == "show_active_rule_interruption_descr" or command == _("show_active_rule_interruption_descr"):
 
       if len(parameters) < 1:
         help_text=_("""This command show description for active rule interruption for user.
 
-  command `show_active_rule_interruption` need 1 params.
+  command `show_active_rule_interruption_descr` need 1 params.
   syntax:
 
-    my_botname_in_this_room: show_active_rule_interruption user_name
+    my_botname_in_this_room: show_active_rule_interruption_descr user_name
 
   example:
-    rsbot: show_active_rule_interruption Baduser
+    rsbot: show_active_rule_interruption_descr Baduser
 
         """)
         if await matrix_api.send_text(room,help_text) == False:
@@ -728,6 +729,101 @@ async def process_command(room,event,commandline):
         if len(rule_interruption_descr_list)==0:
           log.info("no active rule interruptions for user %s"%signature_user_mxid)
           text=_("no active rule interruptions for user %s")%signature_user_mxid
+          if await matrix_api.send_text(room,text) == False:
+            log.error("matrix_api.send_text()")
+            return False
+        else:
+          for item in rule_interruption_descr_list:
+            text = _("""Admin %(admin)s at %(date)s add rule interruption by user %(signature_user_mxid)s with descr: %(descr)s""")%{\
+              "admin":item[0],\
+              "date":item[1],\
+              "signature_user_mxid":signature_user_mxid,\
+              "descr":item[2]\
+              }
+            # показываем отчёт:
+            if await matrix_api.send_text(room,text) == False:
+              log.error("matrix_api.send_text()")
+              return False
+        return True
+
+    elif command == "show_old_rule_interruption_descr" or command == _("show_old_rule_interruption_descr"):
+
+      if len(parameters) < 1:
+        help_text=_("""This command show description for old (not active) rule interruption for user.
+
+  command `show_old_rule_interruption_descr` need 1 params.
+  syntax:
+
+    my_botname_in_this_room: show_old_rule_interruption_descr user_name
+
+  example:
+    rsbot: show_old_rule_interruption_descr Baduser
+
+        """)
+        if await matrix_api.send_text(room,help_text) == False:
+          log.error("matrix_api.send_text()")
+          return False
+        return True
+      else:
+        # параметров достаточно:
+        signature_user = parameters[0]
+
+        if signature_user in room.users:
+          # пользователь указан по MXID:
+          signature_user_mxid = signature_user
+        elif signature_user in room.names:
+          # пользователь указан по имени
+          if len(room.names[signature_user])>1:
+            # несколько пользователей с одинаковыми именами:
+            text=_("""nickname %s not uniqum in this room. Please, select user by mxid (as @user:server.com)""")%signature_user
+            log.warning(text)
+            if await matrix_api.send_text(room,text) == False:
+              log.error("matrix_api.send_text()")
+              return False
+            return True
+          else:
+            # пользователь указан по MXID:
+            signature_user_mxid = room.names[signature_user][0]
+        else:
+          # неизвестный пользователь:
+          text=_("nickname %s not known. Please, correct, or select user by mxid (as @user:server.com)")%signature_user
+          log.warning(text)
+          if await matrix_api.send_text(room,text) == False:
+            log.error("matrix_api.send_text()")
+            return False
+          return True
+        log.debug("signature_user_mxid = %s"%signature_user_mxid)
+
+        ret = sql.check_user_exist(room.room_id, signature_user_mxid)
+        if ret is None:
+          log.error("sql.check_user_exist()")
+          text=_("internal error in function: ") + "sql.check_user_exist()"
+          if await matrix_api.send_text(room,text) == False:
+            log.error("matrix_api.send_text()")
+            return False
+          return False
+        if ret == 0:
+          text = _("no such user in db: %s")%signature_user_mxid
+          log.debug(text)
+          if await matrix_api.send_text(room,text) == False:
+            log.error("matrix_api.send_text()")
+            return False
+          return False
+
+        # получаем данные о записи:
+        # активные:
+        rule_interruption_descr_list = sql.get_rule_interruption_descr(room.room_id, signature_user_mxid,active=False)
+        if rule_interruption_descr_list is None:
+          log.error("sql.get_rule_interruption_descr()")
+          text=_("internal error in function: ") + "sql.get_rule_interruption_descr()"
+          if await matrix_api.send_text(room,text) == False:
+            log.error("matrix_api.send_text()")
+            return False
+          return False
+        # формируем отчёт:
+        if len(rule_interruption_descr_list)==0:
+          log.info("no old rule interruptions for user %s"%signature_user_mxid)
+          text=_("no old rule interruptions for user %s")%signature_user_mxid
           if await matrix_api.send_text(room,text) == False:
             log.error("matrix_api.send_text()")
             return False
